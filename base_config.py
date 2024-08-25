15e8ccaab519f48ec51fa8a24bb6710d2d5dedf4
timomono <166589759+timomono@users.noreply.github.com>
2024-08-25 14:33:38 +0900

    Add docstring

--- base_config.py
+++ base_config.py
@@ -15,23 +15,84 @@ class LocalizationConfig(ABC):
         return "en"
 
 
+"""
+
+"""
+
+
 class BaseConfig(ABC):
+    """Base config class.
+
+    Create a config.py in the project root
+    and create a Config class that extends from BaseConfig.
+    """
+
+    @property
+    @abstractmethod
+    def DEBUG(self) -> bool:
+        """Debug mode.
+
+        Set it to False when deploying.
+        """
+        pass
+
     @property
     @abstractmethod
     def BASE_DIR(self) -> Path:
+        """Project base directory."""
         pass
 
     @property
     def SRC_DIR(self) -> Path:
+        """Source directory.
+
+        Write html template in this folder.
+        """
         return self.BASE_DIR / "src"
 
     @property
     def IGNORE_FILES(self) -> list[Path]:
+        """Files that will not be included in the build.
+
+        Note that extends, includes etc. is possible.
+
+        Folders with the language name used in
+        t_include are automatically ignored.
+        """
         return [
             self.SRC_DIR / "base.html",
         ]
 
     LOCALE_CONFIG: None | LocalizationConfig = None
+    """Localization configuration.
+
+    Set none to turn off localization.
+
+    ### Example
+    ```python
+    class Config(BaseConfig):
+        BASE_DIR = Path(__file__).resolve().parent
+        LOCALE_CONFIG = LocalizationConfig()
+    ```
+    """
+
+
+class MINIFY_CONFIG(ABC):
+    """Minify option.
+    Automatic minification at build time.
+    """
+
+    JSRemoveBr = False
+    """Remove break line for JS.
+
+    Note that a missing semicolon will result in an error.
+    """
+
+    HTMLRemoveBr = False
+    """Remove break line for HTML.
+
+    Note that a missing semicolon will result in an error in script tag.
+    """
 
 
 # class Config:
