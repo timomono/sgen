4f64aa2eeb667efd161719410c5b75cd482c16e5
timomono <166589759+timomono@users.noreply.github.com>
2024-08-25 17:34:34 +0900

    Added pgen command

--- cli.py
+++ cli.py
@@ -20,7 +20,7 @@ def main():
         raise CommandNotFoundError("Command not specified")
     cmd = parseArg()
     if len(cmd) == 0:
-        raise CommandNotFoundError(f"Command \"{sys.argv[1]}\" not found")
+        raise CommandNotFoundError(f'Command "{sys.argv[1]}" not found')
     if len(cmd) != 1:
         raise CommandNameDuplicateError()
     cmd[0].run()
