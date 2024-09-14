6b04390323b8e3b337867e7575bfb42387969d78
timomono <166589759+timomono@users.noreply.github.com>
2024-09-14 15:07:41 +0900

    Add Jinja2 to install_requires

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     console=["cli.py"],
     packages=["sgen"],
+    install_requires=["Jinja2"],
     entry_points={"console_scripts": ["sgen = sgen.cli:main"]},
 )
