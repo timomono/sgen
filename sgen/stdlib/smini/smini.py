2e9d45b90eff447f03413bb557b3b09b12fa6770
timomono <166589759+timomono@users.noreply.github.com>
2024-10-05 15:20:19 +0900

    Ignore important comment (S-Mini)

--- sgen/stdlib/smini/smini.py
+++ sgen/stdlib/smini/smini.py
@@ -1,4 +1,5 @@
 import re
+from typing import Callable
 
 from sgen.stdlib.smini.svg_minify import svg_minify
 
@@ -18,13 +19,37 @@ def minify(
         res = re.sub(r">( |\n|\r\n)+<", "><", res)
         res = re.sub(r"( |\n|\r\n)*(<|>)( |\n|\r\n)*", r"\2", res)
     elif ext == ".css":
-        res = re.sub(r"(\n|\r\n)", "", res)
-        res = re.sub(r" +", " ", res)
-        res = re.sub(r"/\*.*?\*/", "", res)
-        res = re.sub(r" *([:;]) *", r"\1", res)
-        res = re.sub(r" *{ *", "{", res)
-        res = re.sub(r" *} *", "}", res)
-        res = re.sub(r";}", "}", res)
+        # res = re.sub(r"(\n|\r\n)", "", res)
+        # Delete break line except for in important comment
+        def repl_css(
+            regexp: str,
+            replace_str: str | Callable[[re.Match], str],
+            from_: str,
+        ) -> str:
+            return re.sub(
+                r"(?P<comment>/\*\![\s\S]*?\*/)|(?P<r_body>" + regexp + r")",
+                lambda m: (
+                    (replace_str(m) if callable(replace_str) else replace_str)
+                    if m.group("r_body")
+                    else m.group("comment")
+                ),
+                from_,
+            )
+
+        res = repl_css(r"(\n|\r\n)", "", res)
+        # res = re.sub(
+        #     r"(/\*[\s\S]*?\*/)|(\n)",
+        #     lambda m: "" if m.group(2) is "" else m.group(1),
+        #     res,
+        # )
+        res = repl_css(r" +", " ", res)
+        res = repl_css(r"/\*.*?\*/", "", res)
+        res = repl_css(
+            r" *(?P<symbol>[:;,!\*><]) *", lambda m: m.group("symbol"), res
+        )
+        res = repl_css(r" *{ *", "{", res)
+        res = repl_css(r" *} *", "}", res)
+        res = repl_css(r";}", "}", res)
     elif ext == ".js":
         res = "\n".join([s.split("//")[0] for s in re.split("\n|\r\n", res)])
         res = re.sub(r"/\*.*?\*/", "", res)
