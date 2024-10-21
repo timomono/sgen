e854cef456c307b094478acaf5d9aae44710a7a0
timomono <166589759+timomono@users.noreply.github.com>
2024-10-21 20:57:51 +0900

    Updated timeout to be compatible with Windows

--- sgen/components/timeout.py
+++ sgen/components/timeout.py
@@ -1,42 +1,54 @@
-import signal
+import contextlib
+import ctypes
 from typing import Callable
 
-from sgen.components.override_decorator import OverrideStrict, override
+import time
+import threading
 
 
 def timeout(timeout_time: int):
     def wrapper(func: Callable):
         def wrapper2(*args, **kwargs):
-            start_timeout(timeout_time)
-            res = func(*args, **kwargs)
-            finish_timeout()
-            return res
+            with timeout_context(timeout_time):
+                return func(*args, **kwargs)
 
         return wrapper2
 
     return wrapper
 
 
-def start_timeout(timeout_time: int):
-    signal.signal(
-        signal.SIGALRM, lambda s, f: (_ for _ in ()).throw(TimeoutError())
-    )
-    signal.alarm(timeout_time)
+@contextlib.contextmanager
+def timeout_context(timeout_time):
+    thread_id = ctypes.c_long(threading.get_ident())
+
+    def raise_timeout():
+        modified_thread_state_nums = (
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(
+                thread_id, ctypes.py_object(TimeoutError)
+            )
+        )
+        if modified_thread_state_nums == 0:
+            raise ValueError(f"Invalid thread id ({thread_id})")
+        elif modified_thread_state_nums > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            raise SystemError("PyThreadState_SetAsyncExc failure.")
+
+    timer = threading.Timer(timeout_time, raise_timeout)
+    timer.daemon = True
+    timer.start()
+    try:
+        yield
+    finally:
+        timer.cancel()
+        timer.join()
 
 
-def finish_timeout():
-    signal.alarm(0)
+if __name__ == "__main__":
 
+    @timeout(3)
+    def main():
+        print("sleeping")
+        time.sleep(10)
+        print("I slept well...")
 
-class Timeout(OverrideStrict):
-    @override
-    def __init__(self, timeout_time: int) -> None:
-        self.timeout_time = timeout_time
-        super().__init__()
-
-    def __enter__(self):
-        start_timeout(self.timeout_time)
-        return
-
-    def __exit__(self, *args):
-        finish_timeout()
+    main()
