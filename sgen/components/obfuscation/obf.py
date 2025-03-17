d20796c3c1e61be7feabd6d3fa41356f80230ae3
timomono <166589759+timomono@users.noreply.github.com>
2025-03-17 18:25:15 +0900

    [SOBF] Fixed bug that embedded js was not obfuscated.

--- sgen/components/obfuscation/obf.py
+++ sgen/components/obfuscation/obf.py
@@ -57,22 +57,30 @@ similar_texts = (("l", "I"), ("1", "l"), ("1", "I"), ("i", "j"), ("o", "c"))
 #     SYMBOL_MAP[k] = re.sub(r"([0-9])", lambda m: SYMBOL_MAP[m.group(1)], v)
 
 
-def _obfuscate_variable_name(similar_to: Iterable[str] | None = None) -> str:
+def _obfuscate_variable_name(
+    similar_to: Iterable[str] | None = None,
+    seed: int | float | str | bytes | bytearray = None,
+) -> str:
+    # assert seed is not None
+    # random.seed(seed)
+    # res = "".join(random.choices(string.ascii_letters, k=8))
+    # print(seed, res)
+    # return res
     if similar_to:
         for name in similar_to:
             for i in range(len(name)):
                 namelist = list(name)
-                if namelist[i] in (
-                    element
-                    for inner_tuple in similar_texts
-                    for element in inner_tuple
-                ):
-                    try:
-                        namelist[i] = dict(similar_texts)[namelist[i]]
-                    except KeyError:
-                        namelist[i] = dict(
-                            {v: k for k, v in dict(similar_texts).items()}
-                        )[namelist[i]]
+                # if namelist[i] in (
+                #     element
+                #     for inner_tuple in similar_texts
+                #     for element in inner_tuple
+                # ):
+                #     try:
+                #         namelist[i] = dict(similar_texts)[namelist[i]]
+                #     except KeyError:
+                #         namelist[i] = dict(
+                #             {v: k for k, v in dict(similar_texts).items()}
+                #         )[namelist[i]]
                 # Update lower/upper
                 if i == 0:
                     continue
@@ -82,11 +90,15 @@ def _obfuscate_variable_name(similar_to: Iterable[str] | None = None) -> str:
                 name = "".join(namelist)
                 if name not in similar_to:
                     return name
+    random.seed(seed)
     while similar_to is not None and (
         (text := "".join(random.choices(string.ascii_letters, k=8)))
         in similar_to
     ):
+        seed += 1
+        random.seed(seed)
         pass
+    random.seed(seed)
     return (
         "".join(random.choices(string.ascii_letters, k=8))
         if similar_to is None
@@ -94,12 +106,35 @@ def _obfuscate_variable_name(similar_to: Iterable[str] | None = None) -> str:
     )
 
 
-def _split_string(text, max_len: int | None = None) -> list[str]:
+def _split_string(
+    text,
+    max_len: int | None = None,
+    except_embedded_js: bool = False,
+    seed: int = None,
+) -> list[str]:
+    random.seed(seed)
     max_len = max_len or random.randint(1, 10)
-    return [text[i : i + max_len] for i in range(0, len(text), max_len)]
+    if len(text) == 0:
+        random.seed(seed)
+        return [""] * random.randint(1, max_len)
+    if except_embedded_js:
+        parts = RE_EMBEDDED_JS.split(text)
+        embedded_js = RE_EMBEDDED_JS.findall(text)
+        result = []
+        for i, part in enumerate(parts):
+            result.extend(
+                [part[i : i + max_len] for i in range(0, len(part), max_len)]
+            )
+            try:
+                result.append(embedded_js[i])
+            except IndexError:
+                pass
+        return result
+    else:
+        return [text[i : i + max_len] for i in range(0, len(text), max_len)]
 
 
-def _shuffle_functions(code: str) -> str:
+def _shuffle_functions(code: str, seed: int = None) -> str:
     # print(code)
     # return code
     functions: list[str] = re.findall(
@@ -115,6 +150,7 @@ def _shuffle_functions(code: str) -> str:
     for function in functions:
         code = code.replace(function, "")
 
+    random.seed(seed)
     random.shuffle(functions)
     code = "\n".join(functions) + code
     return code
@@ -126,9 +162,11 @@ RE_VARIABLE_NAME = re.compile(
 
 RE_MULTIPLE_BREAK = re.compile(r"\n+")
 
-RE_STRING_LITERAL = re.compile(r'(["\'])(.*?)(["\'])')
+# RE_STRING_LITERAL = re.compile(r'(["\'])(.*?\1)|`[\s\S]*?`')
+RE_STRING_LITERAL = re.compile(r'(["\'`])([\s\S]*?)(\1)')
 
 RE_DICT_KEY = re.compile(r'["\'](\w+)"\s*:')
+RE_EMBEDDED_JS = re.compile(r"\${[\s\S]*?}")
 
 
 class ObfuscateOptions(TypedDict):
@@ -150,19 +188,16 @@ def obfuscate_js(
     code,
     context: ObfuscateContext | None = None,
     **options: Unpack[ObfuscateOptions],
-) -> tuple[str, dict[str, str]]:
+) -> str:
+    # return code
     variable_map = context.variable_map if context is not None else {}
     string_map = context.string_map if context is not None else {}
     # print(context.variable_map, context.string_map)
-    already_declared_vars = list(variable_map.values()) + list(
-        string_map.keys()
-    )
     # variable_map = context.variable_map or {}
 
     # Optional args: https://github.com/python/mypy/issues/6131
     options["is_minify"] = options.get("is_minify", True)
 
-    random.seed(options.get("seed"))
     # Update . to []
     # code = repl_js(r"(\w+)\.(\w+)", r'\1["\2"]', code)
 
@@ -170,56 +205,123 @@ def obfuscate_js(
     code = RE_DICT_KEY.sub(r'["\1"]:', code)
 
     # Update string to function
-    for i in range(random.randint(1, 3)):
+    # for i in range(random.randint(1, 3)):
+    for i in range(1):
         string_literals = RE_STRING_LITERAL.findall(code)
-        # string_map: dict[str, str] = {}
+        string_map_used: dict[str, str] = {}  # {func_name: return_value}
+        string_map_used_quotes: dict[str, str] = {}  # {func_name: quote}
+        args: dict[str, dict[str, str]] = (
+            {}
+        )  # {func_name: {embedded_js: obfuscated_name}}
         for quote, value, quote_end in string_literals:
-            parts = _split_string(value)
+            assert quote in ('"', "'", "`")
+            parts = _split_string(
+                value,
+                except_embedded_js=quote == "`",
+                seed=options.get("seed"),
+            )
+            assert parts != []
             func_calls = []
             for part in parts:
-                if part not in string_map.values():
-                    func_name = _obfuscate_variable_name(
-                        list(variable_map.values()) + list(string_map.keys())
+                # if random.randint(0, 7) == 0:
+                if part in string_map.values() and (
+                    string_map_used_quotes.get(
+                        func_name := list(string_map.keys())[
+                            list(string_map.values()).index(part)
+                        ],
+                        None,
                     )
-                    # print(
-                    #     list(variable_map.values()) + list(string_map.keys()),
-                    #     func_name,
-                    # )
-                    string_map[func_name] = part
+                    == quote
+                ):
+                    # Use existing function
+                    string_map_used[func_name] = part
                 else:
-                    func_name = list(string_map.keys())[
-                        list(string_map.values()).index(part)
-                    ]
+                    # Make new function
+                    func_name = _obfuscate_variable_name(
+                        list(variable_map.values()) + list(string_map.keys()),
+                        seed=options.get("seed"),
+                    )
+                    args[func_name] = {}
+                    # Ensure that the function name is unique
+                    assert func_name not in list(variable_map.values()) + list(
+                        string_map.keys()
+                    ), "Function name already exists"
+                    string_map[func_name] = part
+                    string_map_used[func_name] = part
+                    string_map_used_quotes[func_name] = quote
                 func_calls.append(f"{func_name}()")
+            assert func_calls != []
+            print("REPL", f"{quote}{value}{quote_end}", "+".join(func_calls))
+            print("========CODE========\n", code, "\n====================")
+            # TODO: Not Working:
+            # "hello";'"hello"'
+            # Working:
+            # '"hello"'
             code = code.replace(
                 f"{quote}{value}{quote_end}", "+".join(func_calls)
             )
 
-        for func_name, return_value in string_map.items():
-            if func_name in already_declared_vars:
-                continue
+        for func_name, return_value in string_map_used.items():
+            random.seed(options.get("seed"))
+            # return_value = return_value.replace('"', '\\"')
+            quote = string_map_used_quotes[func_name]
+            # quote = "'"
+            assert quote in ('"', "'", "`")
+
+            # Add embedded js to args
+            if quote == "`":
+                for embedded_js in RE_EMBEDDED_JS.findall(return_value):
+                    # {'RNvNaVop': {'${animal["dog"]}': 'RNvnaVoP'}, ...}
+                    args[func_name][embedded_js] = _obfuscate_variable_name(
+                        list(variable_map.values()) + list(string_map.keys()),
+                        seed=options.get("seed"),
+                    )
+                    padding = " " * (
+                        max(0, 60 - len(return_value.replace("\n", "\\n")))
+                    )
+                    # print(return_value)
+                    print(
+                        "RET"
+                        + return_value.replace("\n", "\\n")
+                        + f"{padding}~~~~~"
+                        + "EMB"
+                        + embedded_js.replace("\n", "\\n")
+                        + f"\tREPLTO ${{{args[func_name][embedded_js]}()}}"
+                    )
+                    # return_value = return_value.replace(
+                    #     embedded_js, f"${{{func_name}[embedded_js]}}"
+                    # )
+                    return_value = return_value.replace(
+                        embedded_js, f"${{{args[func_name][embedded_js]}()}}"
+                    )
+                    # print(return_value)
+
             if random.randint(0, 3) == 0:
                 code = (
-                    f'function {func_name}(){{return "{return_value}"}}\n'
-                    + code
+                    f"function {func_name}"
+                    f"({",".join(args[func_name].values())}){{"
+                    f"return {quote}{return_value}{quote}"
+                    f"}}\n" + code
                 )
             else:
                 code = (
-                    f'{random.choice(("const ", "let ", "", "", ""))}'
-                    f'{func_name}=()=>"{return_value}";\n' + code
+                    # f'{random.choice(("const ", "let ", "", "", ""))}'
+                    f"{func_name}=("
+                    f"{",".join(args[func_name].values())}"
+                    f")=>{quote}{return_value}{quote};\n" + code
                 )
 
     # Update variable name to random name
-    names: list[str] = RE_VARIABLE_NAME.findall(code)
-    # variable_map: dict[str, str] = {}
-    for name in names:
-        try:
-            variable_map[name[0] or name[1]]
-        except KeyError:
-            variable_map[name[0] or name[1]] = _obfuscate_variable_name(
-                list(variable_map.values()) + list(string_map.keys())
-            )
-            # print(list(variable_map.values()) + list(string_map.keys()))
+    # names: list[str] = RE_VARIABLE_NAME.findall(code)
+    # # variable_map: dict[str, str] = {}
+    # for name in names:
+    #     try:
+    #         variable_map[name[0] or name[1]]
+    #     except KeyError:
+    #         variable_map[name[0] or name[1]] = _obfuscate_variable_name(
+    #             list(variable_map.values()) + list(string_map.keys())
+    #         )
+    # print(list(variable_map.values()) + list(string_map.keys()))
 
     # def replace(match):
     #     code = match.group(0)
@@ -233,11 +335,11 @@ def obfuscate_js(
 
     # code = re.sub(r'(["\'].*?["\'])|([^"\']+)', replace, code)
 
-    for original, obfuscated in variable_map.items():
-        code = repl_js(rf"\b{original}\b", obfuscated, code)
+    # for original, obfuscated in variable_map.items():
+    #     code = repl_js(rf"\b{original}\b", obfuscated, code)
 
     # Shuffle functions
-    code = _shuffle_functions(code)
+    # code = _shuffle_functions(code, seed=options.get("seed"))
 
     # Minify
     if options["is_minify"]:
@@ -247,7 +349,10 @@ def obfuscate_js(
 
     # print(context.variable_map, context.string_map, string_map)
 
-    return code  # type:ignore
+    # print("variable_map", variable_map)
+    # print("already_declared_vars", already_declared_vars)
+    # print("string_map", string_map)
+    return code
 
 
 if __name__ == "__main__":
