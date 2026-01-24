b95a493267dbc2a206843336db0c8275594fdc1f
timomono <166589759+timomono@users.noreply.github.com>
2026-01-24 21:28:02 +0900

    add tailwind.config.js in the template

---
+++ sgen/templates/project/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "primary-color": "var(--primary-color)",
+                "secondary-color": "var(--secondary-color)"
+            },
+        },
+    },
+}
