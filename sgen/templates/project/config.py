e36087dbe64abeff9f0726bd94f6d846f4effe5e
timomono <166589759+timomono@users.noreply.github.com>
2026-02-26 21:15:49 +0900

    make log level changable with env variable SGEN_LOG_LEVEL

--- sgen/templates/project/config.py
+++ sgen/templates/project/config.py
@@ -1,3 +1,5 @@
+from logging import basicConfig
+from os import environ
 from pathlib import Path
 from sgen.components.override_decorator import override
 from sgen.base_config import BaseConfig
@@ -5,6 +7,8 @@ from sgen.cli import running_command
 from sgen.cmds import ListenChange
 from sgen.stdlib.smini.middleware import SminiMiddleware
 from sgen.stdlib.xmlsitemap.middleware import XMLSitemapMiddleware
+
+basicConfig(encoding="utf-8", level=environ.get("SGEN_LOG_LEVEL", "INFO"))
 
 
 class Config(BaseConfig):
