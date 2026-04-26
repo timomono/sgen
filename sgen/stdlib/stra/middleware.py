efe69c74a21f490d5e5df7a94a3865f0d2193f74
timomono <166589759+timomono@users.noreply.github.com>
2026-04-26 18:51:06 +0900

    Add noindex to redirect_locale.html

--- sgen/stdlib/stra/middleware.py
+++ sgen/stdlib/stra/middleware.py
@@ -275,6 +275,7 @@ def localeRedirectIndex(localesStr: str, config: StraConfig) -> str:
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex,nofollow">
     <title>Redirecting...</title>
 </head>
 <body>
