4764506cf9ee813ad1a58511cdbbde11d890141e
timomono <166589759+timomono@users.noreply.github.com>
2026-05-24 18:15:27 +0900

    Refactor PathMiddleware to accept a path replacer function and enhance filepath template tag with middleware check

--- sgen/stdlib/path/middleware.py
+++ sgen/stdlib/path/middleware.py
@@ -2,9 +2,17 @@ from pathlib import Path
 
 from sgen.base_middleware import BaseMiddleware
 from sgen.components.override_decorator import override
+from typing import Callable
 
 
 class PathMiddleware(BaseMiddleware):
+    @override
+    def __init__(
+        self, path_replacer: Callable[[str], str] = lambda x: x
+    ) -> None:
+        self.path_replacer = path_replacer
+        super().__init__()
+
     @override
     def after(self, build_path: Path) -> None:
         from sgen.get_config import sgen_config
@@ -15,7 +23,9 @@ class PathMiddleware(BaseMiddleware):
             body = file.read_bytes()
             body = body.replace(
                 b"[[path here]]",
-                str(file.relative_to(sgen_config.BUILD_DIR)).encode(),
+                self.path_replacer(
+                    str(file.relative_to(sgen_config.BUILD_DIR))
+                ).encode(),
             )
             if body.find(b"path here") != -1:
                 print(body.decode(errors="replace"))
